9627cd962b27631823ee6a118d2421653b8545fc
bder00s <105774363+bder00s@users.noreply.github.com>
2023-05-01 09:43:16 +0200

    Initial commit

---
+++ src/main/java/nl/novi/teacherproject/model/Teacher.java
@@ -0,0 +1,45 @@
+package nl.novi.teacherproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "teachers")
+
+public class Teacher {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    
+
+    /// GETTERS AND SETTERS
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
